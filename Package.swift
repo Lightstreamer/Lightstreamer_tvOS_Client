f76c394837cba71985a438bbf91b85cfbcf76930
Vincenzo D'Ippolito <vincenzo.dippolito@lightstreamer.com>
2020-12-10 11:18:13 +0100

    First commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+//
+// Copyright (c) Lightstreamer Srl
+// See LICENSE.md for license terms
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Lightstreamer_tvOS_Client",
+    products: [
+        .library(
+            name: "Lightstreamer_tvOS_Client", 
+            targets: ["Lightstreamer_tvOS_Client"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "Lightstreamer_tvOS_Client", 
+            url: "https://www.lightstreamer.com/repo/cocoapods/ls-tvos-client/4.3.0/ls-tvos-client-4.3.0.zip", 
+            checksum: "c7cbf16b2de9e327f69788ca2d5df5ee61cb9db2995b7a3c821ad1b4738ae684"
+        )
+    ]
+)
