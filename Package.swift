61d47986c969e7e84168596f4e41840086ab826a
Dario Crivelli <dario.crivelli@lightstreamer.com>
2022-03-09 10:08:49 +0100

    Use links in the new form.

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Lightstreamer_tvOS_Client", 
-            url: "https://www.lightstreamer.com/repo/cocoapods/ls-tvos-client/4.3.2/ls-tvos-client-4.3.2.zip", 
+            url: "https://sdk.lightstreamer.com/ls-tvos-client/4.3.2/lib/ls-tvos-client-4.3.2.zip", 
             checksum: "b886c14f92e6529cf202b0b8f485037c2b398dc8976282c3ba46858ee9a74bbf"
         )
     ]
